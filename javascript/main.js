1fbd04a0420a23a977ca3c34e276f7660c72e6eb
AlexandreZenas <94380403+AlexandreZenas@users.noreply.github.com>
2022-11-01 04:47:43 -0300

    css & js commit

---
+++ javascript/main.js
@@ -0,0 +1,20 @@
+var navAnimation = document.querySelector('nav');
+function navFunction(){
+    navAnimation.classList.remove('hidden');
+};
+setTimeout(navFunction, 1080)
+
+
+//seletor de cores aleatórias
+var logo = document.querySelectorAll('.navLogo')[0];
+var h1ContainerHome = document.querySelectorAll('.containerHome h1')[0];
+
+logo.addEventListener('click',()=>{
+    const [red, green, blue] = [
+        Math.floor(Math.random() * 255),
+        Math.floor(Math.random() * 255),
+        Math.floor(Math.random() * 255)        
+    ]
+    h1ContainerHome.style.color =`rgb(${red},${green},${blue})`;
+})
+var mudanceGit
